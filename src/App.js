4f0692ef180ee7a82917581e97e9fc378e1a5206
Arjun3492 <arjun.kohli.330@gmail.com>
2022-07-20 22:33:10 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { useEffect } from 'react';
 import CovidSummary from './component/CovidSummary';
 import LineGraph from './component/LineGraph';
-  import axios from 'axios';
- function App() {
+import axios from './axios';
+function App() {
   useEffect(() => {
-  axios.get(`/summary`).then(res=>{console.log(res);}).catch(err=>{console.log(err)})},[]);
-  
+    axios.get(`/summary`).then(res => { console.log(res); }).catch(err => { console.log(err) })
+  }, []);
+
   return (
     <div>
-      
-     <CovidSummary
-     TotalConfirmed={0}
-        TotalRecovered ={0}
+
+      <CovidSummary
+        TotalConfirmed={0}
+        TotalRecovered={0}
         TotalDeath={0}
-        counrty= {'INDIA'}
-     />
-       <LineGraph/>
-       
+        counrty={'INDIA'}
+      />
+      <LineGraph />
+
     </div>
   )
 }
 
 export default App
- 
- 
+
--- src/component/CovidSummary.js
+++ src/component/CovidSummary.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import Card from './component/Card';
+import Card from './Card';
 
- const CovidSummary=(props)=> {
-    const{
-        TotalConfirmed,
-        TotalRecovered,
-        TotalDeath,
-        counrty,
-    }=props;
+const CovidSummary = (props) => {
+  const {
+    TotalConfirmed,
+    TotalRecovered,
+    TotalDeath,
+    counrty,
+  } = props;
   return (
     <div>
-        <h1>{counrty===""?'World Wide Corona Report':'country'}</h1>
-         <div style={{display: 'flex', justifyContent: 'center'}}>
-      <Card><span> Total Confirmed</span><br/><span>{TotalConfirmed}</span></Card>
-      <Card><span> Total Recovered</span><br/><span>{TotalRecovered}</span></Card>
-      <Card><span>Total Death</span><br/><span>{TotalDeath}</span></Card>
-     </div>
+      <h1>{counrty === "" ? 'World Wide Corona Report' : 'country'}</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Card><span> Total Confirmed</span><br /><span>{TotalConfirmed}</span></Card>
+        <Card><span> Total Recovered</span><br /><span>{TotalRecovered}</span></Card>
+        <Card><span>Total Death</span><br /><span>{TotalDeath}</span></Card>
+      </div>
     </div>
   )
 }
